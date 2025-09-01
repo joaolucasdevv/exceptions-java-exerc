2c6868df9345da2dcc05ce0830caa53e00c46724
Joao Lucas <jldecristo@gmail.com>
2025-09-01 14:10:38 -0300

    getters and setters

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,9 +4,9 @@ import java.time.LocalDate;
 
 public class Reservation {
 
-    public Integer roomNumber;
-    public LocalDate checkIn;
-    public LocalDate checkOut;
+    private Integer roomNumber;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
 
     public Reservation() {
     }
@@ -14,6 +14,30 @@ public class Reservation {
     public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public void setCheckIn(LocalDate checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public void setCheckOut(LocalDate checkOut) {
         this.checkOut = checkOut;
     }
 
