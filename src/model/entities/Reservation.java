60066d927efa4e15cb8819c35cfde9d84719aba9
Joao Lucas <jldecristo@gmail.com>
2025-09-01 14:06:36 -0300

    construtores de vez

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -5,16 +5,16 @@ import java.time.LocalDate;
 public class Reservation {
 
     public Integer roomNumber;
-    public LocalDate checkin;
-    public LocalDate checkout;
+    public LocalDate checkIn;
+    public LocalDate checkOut;
 
     public Reservation() {
     }
 
-    public Reservation(Integer roomNumber, LocalDate checkin, LocalDate checkout) {
+    public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
         this.roomNumber = roomNumber;
-        this.checkin = checkin;
-        this.checkout = checkout;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
 }
