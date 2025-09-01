8f17eaaaece791c00e4c298dd3b9a22dde086525
Joao Lucas <jldecristo@gmail.com>
2025-09-01 14:02:54 -0300

    atributos de Reservation

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,4 +1,13 @@
 package model.entities;
 
+import java.time.LocalDate;
+
 public class Reservation {
+
+    public Integer roomNumber;
+    public LocalDate checkin;
+    public LocalDate checkout;
+
+
+
 }
