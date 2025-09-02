c99bf5f7405cf5203274d819a297d077a7a02b6a
Joao Lucas <jldecristo@gmail.com>
2025-09-02 11:46:33 -0300

    construtores

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import model.entities.Reservation;
+
+import java.time.LocalDate;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,8 +12,16 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Room number: ");
+        int roomNumber = sc.nextInt();
 
+        System.out.print("Check-in date (dd/MM/yyyy): ");
+        LocalDate checkIn = LocalDate.parse(sc.nextLine(), Reservation.DTF);
 
+        System.out.print("Check-out date (dd/MM/yyyy): ");
+        LocalDate checkOut = LocalDate.parse(sc.nextLine(), Reservation.DTF);
+
+        Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
 
 
 
